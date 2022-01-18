9ce65bdeea78b5f3df25a06d4f7f227e694ab3c9
Rohit <rohitamar@gmail.com>
2022-01-18 11:34:42 -0500

    first

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,4 @@ import ReactDOM from 'react-dom';
 import './styles/EmbodyApp.css';
 import EmbodyApp from './components/EmbodyApp';
 
-//import * as serviceWorker from './serviceWorker';
-
 ReactDOM.render(<EmbodyApp />, document.getElementById('root'));
