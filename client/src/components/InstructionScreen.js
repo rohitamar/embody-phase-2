1fa52d98d6d800ec33095faf449218b8a7ad18b8
Rohit <rohitamar@gmail.com>
2022-02-02 21:16:53 -0500

    random

--- client/src/components/InstructionScreen.js
+++ client/src/components/InstructionScreen.js
@@ -12,7 +12,7 @@ class InstructionScreen extends React.Component {
         return (
             <div className = "InstructionScreen__wrapper">
                 <div className = "InstructionScreen__title">
-                    Instructions for participants are flexible and will be adapted depending on the experiment. Experimental conditions and titles during the task are also flexible.
+                    Instructions for participants are flexible and will be adapted depending on the experiment Experimental conditions and titles during the task are also flexible.
                 </div>
                 <NavLink 
                     className = "InstructionScreen__button"
