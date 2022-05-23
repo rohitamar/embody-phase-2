354b541a9671a2e259f0418d3fee49908763a7b9
Rohit <rohitamar@gmail.com>
2022-05-23 11:57:15 -0400

    some final changes

--- client/src/components/InstructionScreen.js
+++ client/src/components/InstructionScreen.js
@@ -8,28 +8,29 @@ class InstructionScreen extends React.Component {
     constructor(props) {
         super(props);
         this.cookies = new Cookies();
-        this.firstSessionInstruction = "Please read the instructions carefully. You will first drink some water."
-        + "Then, you will be presented with pictures of two blank human bodies."
-        + "Think carefully what you feel in your body, your task is to color the bodily regions whose activity you feel"
-        + "changing after drinking water. For the first body, color the regions whose activity you feel increasing or getting stronger."
+        this.firstSessionInstruction = "Please read the instructions carefully.\n\nYou will first drink some water."
+        + " Then, you will be presented with pictures of two blank human bodies. "
+        + " Think carefully what you feel in your body, your task is to color the bodily regions whose activity you feel "
+        + "changing after drinking water.\n\nFor the first body, color the regions whose activity you feel increasing or getting stronger. "
         + "For the second body, color the regions whose activity you feel decreasing or getting weaker. You can color any region of the bodies you feel appropriate," 
-        + "from the head to the toes using your finger."
+        + " from the head to the toes using your finger. "
         + "When you have finished coloring the bodies,  you can close the tab and continue with the Qualtrics questionnaire."
 
-        this.otherSessionInstruction = "Please read the instructions carefully. Today we are interested in understanding where you feel sensations in your body at the present moment. You will be presented with pictures of two blank human bodies. Think carefully what you feel in your body, your task is to color the bodily regions where you feel activity. For the first body, color the regions whose activity you feel stronger. For the second body, color the regions whose activity you feel weaker. You can color any region of the bodies you feel appropriate, from the head to the toes using your finger. When you have finished coloring the bodies,  you can close the tab and continue with the Qualtrics questionnaire.";
+        this.otherSessionInstruction = "Please read the instructions carefully.\n\nToday we are interested in understanding where you feel sensations in your body at the present moment.\n\nYou will be presented with pictures of two blank human bodies. Think carefully what you feel in your body, your task is to color the bodily regions where you feel activity.\nFor the first body, color the regions whose activity you feel stronger. For the second body, color the regions whose activity you feel weaker. You can color any region of the bodies you feel appropriate, from the head to the toes using your finger. When you have finished coloring the bodies,  you can close the tab and continue with the Qualtrics questionnaire.";
 
-        let determineSession = true;
         this.state = {
-            participantID: this.cookies.get("participantID")
+            participantID: this.cookies.get("participantID"),
+            determineSession: true
         };
 
         axios.get('https://bodily-maps.herokuapp.com/participant/find', {
             params: {
-                    id: this.state.participantID
+                id: this.state.participantID
             }
         }).then((res) => {
-            console.log(res);
-            this.determineSession = (res.data.sessionNumber > 1) ? false : true;
+            this.setState({
+                determineSession: (res.data.sessionNumber == 1) 
+            });
         });
     }
 
@@ -39,7 +40,7 @@ class InstructionScreen extends React.Component {
         return (
             <div className = "InstructionScreen__wrapper">
                 <div className = "InstructionScreen__title">
-                    { this.determineSession ? this.firstSessionInstruction : this.otherSessionInstruction }
+                    { this.state.determineSession ? this.firstSessionInstruction : this.otherSessionInstruction }
                 </div>
                 <NavLink 
                     className = "InstructionScreen__button"
